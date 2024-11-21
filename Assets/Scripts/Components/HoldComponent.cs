9738029b207a4a1ad096f12a51a6ef5cdff79ae4
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-11-21 22:02:25 +0100

    Updated package

--- Assets/Scripts/Components/HoldComponent.cs
+++ Assets/Scripts/Components/HoldComponent.cs
@@ -6,17 +6,41 @@ namespace Game
     public class HoldComponent : MonoBehaviour
     {
         [SerializeField] private MouseDragHandler _handler;
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private float _force = 20.0f;
+        [SerializeField] private AnimationCurve _velocityCurve;
 
         private Vector3 _offset;
+        private Vector3 _target;
 
         private void OnDragBegin(MouseEventData data)
         {
-            _offset = Quaternion.Inverse(data.camera.transform.rotation) * (data.source.position - data.camera.transform.position);
+            var rotator = data.camera.transform;
+            _offset = Quaternion.Inverse(rotator.rotation) * (transform.position - rotator.position);
+            _target = rotator.position + rotator.rotation * _offset;
         }
 
         private void OnDrag(MouseEventData data)
         {
-            data.source.position = data.camera.transform.position + data.camera.transform.rotation * _offset;
+            var rotator = data.camera.transform;
+            _target = rotator.position + rotator.rotation * _offset;
+
+            ApplyForce();
+        }
+
+        private void ApplyForce()
+        {
+            var delta = _target - transform.position;
+            _rigidbody.AddForce(delta * _force, ForceMode.Force);
+
+            var velocityTime = Mathf.Clamp01(delta.magnitude / _offset.magnitude);
+            var velocity = _velocityCurve.Evaluate(velocityTime);
+            _rigidbody.velocity = ClampVelocity(_rigidbody.velocity, velocity);
+        }
+
+        private Vector3 ClampVelocity(Vector3 velocity, float maxVelocity)
+        {
+            return velocity.normalized * Mathf.Min(velocity.magnitude, maxVelocity);
         }
 
         private void Awake()
