b6158c6333015ba5a387db974ad170197fadc801
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-11-21 22:31:33 +0100

    Added outline

---
+++ Assets/Scripts/Components/OutlineComponent.cs
@@ -0,0 +1,38 @@
+using Common.Coroutines;
+using Common.Materials;
+using UnityEngine;
+
+namespace Game
+{
+    public class OutlineComponent : MonoBehaviour
+    {
+        [SerializeField] private MaterialApplier _applier;
+        [SerializeField] private MaterialAlpha _alpha;
+
+        private int _highlights;
+        private Coroutine _faderoutine;
+
+        public void FadeIn()
+        {
+            if (++_highlights == 1)
+            {
+                _applier.ApplyClone();
+
+                _faderoutine.Stop(this);
+                _faderoutine = Yield.ValueTo(0.0f, 1.0f, _alpha.SetValue, Yield.TimeNormalized(0.15f))
+                    .Start(this);
+            }
+        }
+
+        public void FadeOut()
+        {
+            if (--_highlights == 0)
+            {
+                _faderoutine.Stop(this);
+                _faderoutine = Yield.ValueTo(1.0f, 0.0f, _alpha.SetValue, Yield.TimeNormalized(0.15f))
+                    .Then(_applier.ClearClone)
+                    .Start(this);
+            }
+        }
+    }
+}
