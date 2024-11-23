004e74f28b17a8014498a9b20d9cf50109560786
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-11-23 11:33:35 +0100

    Update AnimationsPlayComponent.cs

--- Assets/Scripts/Components/AnimationsPlayComponent.cs
+++ Assets/Scripts/Components/AnimationsPlayComponent.cs
@@ -5,18 +5,12 @@ namespace Game
     public class AnimationsPlayComponent : MonoBehaviour
     {
         [SerializeField] private Animation[] _animations;
-        [SerializeField] private string _name;
 
         public void PlayAll()
         {
-            PlayAll(_name);
-        }
-
-        public void PlayAll(string name)
-        {
             foreach (var animation in _animations)
             {
-                animation.Play(name);
+                animation.Play();
             }
         }
 
